2771c90351a4b9f9fe916c560940f53568dd8fd3
zuzudacootkid <>
2021-11-29 22:00:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import random
+
+
+names_list = []
+asked = []
+score=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+     root = Tk()
+     root.title("Mental Health Quiz")
+     root.geometry("500x500")
+     class_object = Starter(root)
+     root.mainloop()
